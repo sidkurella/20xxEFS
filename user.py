82c9ca5dad34cb4dd2d2e558f2bbc49a0e18728e
Siddharth Kurella <siddharthkurella@gmail.com>
2018-12-05 01:58:21 -0800

    Change user serialization format to pickle
    Makes dealing with str/bytes easier since root dir must be a str

--- user.py
+++ user.py
@@ -1,20 +1,20 @@
-import base64
 import collections
 import json
 import os
 import os.path
+import pickle
 
 from Crypto import Random
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES
 
 from pyefs.name_gen import NameGenerator
-from pyefs.conf import SECURITY_PARAM
+from pyefs.conf import SECURITY_PARAM, AES_KEY_SZ
 
 class User:
     @staticmethod
-    def parse(kvs):
-        return User(**kvs)
+    def parse(data):
+        return pickle.loads(data)
 
     @staticmethod
     def generate(ks=None):
@@ -25,12 +25,12 @@ class User:
         pub_key = rsa_key.publickey()
 
         return User(
-            sym_k   = Random.new().read(AES.block_size),
+            sym_k   = Random.new().read(AES_KEY_SZ),
             asym_pk = pub_key.export_key(),
             asym_sk = rsa_key.export_key(),
             sign_pk = pub_key.export_key(),
             sign_sk = rsa_key.export_key(),
-            root    = NameGenerator.random_filename().encode('ascii')
+            root    = NameGenerator.random_filename()
         )
 
     def __init__(self,
@@ -46,10 +46,7 @@ class User:
         self.root    = root
 
     def format(self):
-        exports = [ 'sym_k', 'asym_pk', 'asym_sk', 'sign_pk', 'sign_sk', 'root' ]
-
-        return { k: base64.b64encode(getattr(self, k)).decode('ascii')
-                        for k in exports }
+        return pickle.dumps(self)
 
 class UserStore:
     def __init__(self, path):
@@ -62,14 +59,14 @@ class UserStore:
     def _get_user_file(self, name):
         return os.path.join(self.path, name)
     def _get_user_kvs(self, name):
-        with open(self._get_user_file(name), 'r') as f:
-            return { k: base64.b64decode(v) for k, v in json.load(f).items() }
+        with open(self._get_user_file(name), 'rb') as f:
+            return User.parse(f.read())
 
     def get_user(self, name):
         if name not in self.cache:
-            self.cache[name] = User.parse(self._get_user_kvs(name))
+            self.cache[name] = self._get_user_kvs(name)
         return self.cache[name]
 
     def add_user(self, name, user):
-        with open(self._get_user_file(name), 'w') as f:
-            json.dump(user.format(), f)
+        with open(self._get_user_file(name), 'wb') as f:
+            f.write(user.format())
